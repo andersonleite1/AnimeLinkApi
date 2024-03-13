aea2b7914a9ba4600305ee1b0188bd252639cc75
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-13 05:36:37 -0300

    chore: start project

---
+++ src/main/java/br/com/andersonleite/animelinkapi/AnimeLinkApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersonleite.animelinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeLinkApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnimeLinkApiApplication.class, args);
+  }
+
+}
