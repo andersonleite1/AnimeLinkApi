d8c7b0518c8ddbbb6cd04a9fc06e89652229d27a
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-13 07:06:41 -0300

    chore: add connection with database mysql

--- src/main/java/br/com/andersonleite/animelinkapi/domain/Anime.java
+++ src/main/java/br/com/andersonleite/animelinkapi/domain/Anime.java
@@ -1,11 +1,21 @@
 package br.com.andersonleite.animelinkapi.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 
