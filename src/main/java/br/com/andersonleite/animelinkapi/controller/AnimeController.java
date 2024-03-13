2d2418ae7c111ffe551dde75031560e12f6c5317
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-13 06:32:35 -0300

    feat: add method AnimeService and AnimeController for delete animes

--- src/main/java/br/com/andersonleite/animelinkapi/controller/AnimeController.java
+++ src/main/java/br/com/andersonleite/animelinkapi/controller/AnimeController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,13 +28,18 @@ public class AnimeController {
   }
 
   @GetMapping(path = "/{id}")
-  public ResponseEntity<Anime> findById(@PathVariable long id){
+  public ResponseEntity<Anime> findById(@PathVariable long id) {
     return ResponseEntity.ok(animeService.findById(id));
   }
 
   @PostMapping
-  public ResponseEntity<Anime> save(@RequestBody Anime anime){
+  public ResponseEntity<Anime> save(@RequestBody Anime anime) {
     return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
   }
 
+  @DeleteMapping(path = "/{id}")
+  public ResponseEntity<Void> delete(@PathVariable long id) {
+    animeService.delete(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 }
