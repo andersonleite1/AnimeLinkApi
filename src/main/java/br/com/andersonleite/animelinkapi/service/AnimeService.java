b72b3198b44cc22b95d16ba1cfb992f99a050be4
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-13 06:16:39 -0300

    feat: add method AnimeService and AnimeController for get animes by id

--- src/main/java/br/com/andersonleite/animelinkapi/service/AnimeService.java
+++ src/main/java/br/com/andersonleite/animelinkapi/service/AnimeService.java
@@ -2,11 +2,22 @@ package br.com.andersonleite.animelinkapi.service;
 
 import br.com.andersonleite.animelinkapi.domain.Anime;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AnimeService {
+  private List<Anime> animes = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+
   public List<Anime> listAll() {
-    return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+    return animes;
+  }
+
+  public Anime findById(long id) {
+    return animes.stream()
+        .filter(anime -> anime.getId().equals(id))
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
   }
 }
