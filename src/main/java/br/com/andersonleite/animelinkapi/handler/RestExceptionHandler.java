b7a2f98e535d3cade4d5a0426e3c4c96b0082a01
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-14 05:33:12 -0300

    feat: create handler global for BadRequestException

---
+++ src/main/java/br/com/andersonleite/animelinkapi/handler/RestExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.com.andersonleite.animelinkapi.handler;
+
+import br.com.andersonleite.animelinkapi.exception.BadRequestException;
+import br.com.andersonleite.animelinkapi.exception.BadRequestExceptionDetails;
+import java.time.LocalDateTime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException bre){
+    return new ResponseEntity<>(
+        BadRequestExceptionDetails.builder()
+            .timestamp(LocalDateTime.now())
+            .status(HttpStatus.BAD_REQUEST.value())
+            .title("Bad Request Exception, Check the Documentation")
+            .details(bre.getMessage())
+            .developerMessage(bre.getClass().getName())
+            .build(), HttpStatus.BAD_REQUEST);
+  }
+}
