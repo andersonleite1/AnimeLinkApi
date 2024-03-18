8d4c56b574018f0932c1d4abb9e75e2c86c2b63f
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-18 06:01:41 -0300

    feat: add Builder at AnimePostRequestBody and AnimePutRequestBody

--- src/main/java/br/com/andersonleite/animelinkapi/dto/AnimePostRequestBody.java
+++ src/main/java/br/com/andersonleite/animelinkapi/dto/AnimePostRequestBody.java
@@ -1,9 +1,11 @@
 package br.com.andersonleite.animelinkapi.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AnimePostRequestBody {
   @NotEmpty(message = "The anime name cannot be empty")
   private String name;
