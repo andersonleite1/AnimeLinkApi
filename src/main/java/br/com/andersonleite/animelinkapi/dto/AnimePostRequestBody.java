1bc31677aff41a73b75769834e7abb2fe826d625
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-20 05:49:49 -0300

    feat: add annotations at AnimePostRequestBody, AnimePutRequestBody NoArgsConstructor and AllArgsConstructor

--- src/main/java/br/com/andersonleite/animelinkapi/dto/AnimePostRequestBody.java
+++ src/main/java/br/com/andersonleite/animelinkapi/dto/AnimePostRequestBody.java
@@ -1,11 +1,15 @@
 package br.com.andersonleite.animelinkapi.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnimePostRequestBody {
   @NotEmpty(message = "The anime name cannot be empty")
   private String name;
