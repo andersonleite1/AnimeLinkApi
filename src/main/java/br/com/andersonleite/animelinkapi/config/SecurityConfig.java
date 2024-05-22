cc52814e80700fda204e678a4cc3b1e1f91aa8fc
Anderson Leite <andersonleite.dev@gmail.com>
2024-05-22 07:01:23 -0300

    feat: add authentication by auth.userDetailsService

--- src/main/java/br/com/andersonleite/animelinkapi/config/SecurityConfig.java
+++ src/main/java/br/com/andersonleite/animelinkapi/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package br.com.andersonleite.animelinkapi.config;
 
+import br.com.andersonleite.animelinkapi.service.UserDataService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,7 +14,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@SuppressWarnings("java:S5344")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private final UserDataService userDataService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,14 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         log.info("Password encoded {}", passwordEncoder.encode("test"));
-        auth.inMemoryAuthentication()
-                .withUser("anderson")
-                .password(passwordEncoder.encode("pass"))
-                .roles("USER", "ADMIN")
-                .and()
-                .withUser("leite")
-                .password(passwordEncoder.encode("secret_pass"))
-                .roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("anderson")
+//                .password(passwordEncoder.encode("pass"))
+//                .roles("USER", "ADMIN")
+//                .and()
+//                .withUser("leite")
+//                .password(passwordEncoder.encode("secret_pass"))
+//                .roles("USER");
+
+        auth.userDetailsService(userDataService)
+                .passwordEncoder(passwordEncoder);
     }
 }
 
