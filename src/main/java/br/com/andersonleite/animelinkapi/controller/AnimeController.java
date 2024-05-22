d5111ee221fa78b21b4d200be0c6eee238a15bed
Anderson Leite <andersonleite.dev@gmail.com>
2024-05-22 07:00:24 -0300

    feat: add findByIdAuthenticationPrincipal at AnimeController

--- src/main/java/br/com/andersonleite/animelinkapi/controller/AnimeController.java
+++ src/main/java/br/com/andersonleite/animelinkapi/controller/AnimeController.java
@@ -10,6 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -32,23 +35,34 @@ public class AnimeController {
     return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
   }
 
+  @GetMapping(path = "by-id/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
+  public ResponseEntity<Anime> findByIdAuthenticationPrincipal(@PathVariable long id,
+                                                               @AuthenticationPrincipal UserDetails userDetails) {
+    log.info(userDetails);
+    return ResponseEntity.ok(animeService.findByIdOrThrowBadRequestException(id));
+  }
+
   @GetMapping(path = "/find")
   public ResponseEntity<List<Anime>> findByName(@RequestParam String name) {
     return ResponseEntity.ok(animeService.findByName(name));
   }
 
   @PostMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
     return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
   }
 
   @PutMapping
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> replace(@RequestBody AnimePutRequestBody animePutRequestBody) {
     animeService.replace(animePutRequestBody);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
   @DeleteMapping(path = "/{id}")
+  @PreAuthorize("hasRole('ADMIN')")
   public ResponseEntity<Void> delete(@PathVariable long id) {
     animeService.delete(id);
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
