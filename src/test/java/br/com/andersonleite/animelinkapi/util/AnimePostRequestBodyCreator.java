5c6bb2897620dc03c567aac9832b1310e0d64ec1
Anderson Leite <andersonleite.dev@gmail.com>
2024-06-01 20:35:01 -0300

    test: add AnimePostRequestBodyCreator & AnimePutRequestBodyCreator

--- src/test/java/br/com/andersonleite/animelinkapi/util/AnimePostRequestBodyCreator.java
+++ src/test/java/br/com/andersonleite/animelinkapi/util/AnimePostRequestBodyCreator.java
@@ -3,9 +3,15 @@ package br.com.andersonleite.animelinkapi.util;
 import br.com.andersonleite.animelinkapi.dto.AnimePostRequestBody;
 
 public class AnimePostRequestBodyCreator {
-  public static AnimePostRequestBody createAnimePostRequestBody(){
+  public static AnimePostRequestBody createAnimePostRequestBody() {
     return AnimePostRequestBody.builder()
-        .name(AnimeCreator.createAnimeToBeSaved().getName())
-        .build();
+            .name(AnimeCreator.createAnimeToBeSaved().getName())
+            .synopsis(AnimeCreator.createAnimeToBeSaved().getSynopsis())
+            .releaseYear(AnimeCreator.createAnimeToBeSaved().getReleaseYear())
+            .classificationAge(AnimeCreator.createAnimeToBeSaved().getClassificationAge())
+            .author(AnimeCreator.createAnimeToBeSaved().getAuthor())
+            .animationStudio(AnimeCreator.createAnimeToBeSaved().getAnimationStudio())
+            .inProgress(AnimeCreator.createAnimeToBeSaved().isInProgress())
+            .build();
   }
 }
--- src/test/java/br/com/andersonleite/animelinkapi/util/AnimePutRequestBodyCreator.java
+++ src/test/java/br/com/andersonleite/animelinkapi/util/AnimePutRequestBodyCreator.java
@@ -3,10 +3,16 @@ package br.com.andersonleite.animelinkapi.util;
 import br.com.andersonleite.animelinkapi.dto.AnimePutRequestBody;
 
 public class AnimePutRequestBodyCreator {
-  public static AnimePutRequestBody createAnimePutRequestBody(){
+  public static AnimePutRequestBody createAnimePutRequestBody() {
     return AnimePutRequestBody.builder()
-        .id(AnimeCreator.createValidUpdatedAnime().getId())
-        .name(AnimeCreator.createValidUpdatedAnime().getName())
-        .build();
+            .id(AnimeCreator.createValidUpdatedAnime().getId())
+            .name(AnimeCreator.createValidUpdatedAnime().getName())
+            .synopsis(AnimeCreator.createValidUpdatedAnime().getSynopsis())
+            .releaseYear(AnimeCreator.createValidUpdatedAnime().getReleaseYear())
+            .classificationAge(AnimeCreator.createValidUpdatedAnime().getClassificationAge())
+            .author(AnimeCreator.createValidUpdatedAnime().getAuthor())
+            .animationStudio(AnimeCreator.createValidUpdatedAnime().getAnimationStudio())
+            .inProgress(AnimeCreator.createValidUpdatedAnime().isInProgress())
+            .build();
   }
 }
